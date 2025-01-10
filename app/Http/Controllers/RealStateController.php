84245fbe3a847fc4ae8f34eb7e93bdfb7451260e
mohamed <mohamedmld954@gmail.com>
2025-01-11 00:01:12 +0100

    first commit

---
+++ app/Http/Controllers/RealStateController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RealState;
+use App\Http\Requests\StoreRealStateRequest;
+use App\Http\Requests\UpdateRealStateRequest;
+
+class RealStateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRealStateRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RealState $realState)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RealState $realState)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRealStateRequest $request, RealState $realState)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RealState $realState)
+    {
+        //
+    }
+}
---
+++ app/Models/RealState.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RealState extends Model
+{
+    /** @use HasFactory<\Database\Factories\RealStateFactory> */
+    use HasFactory;
+}
